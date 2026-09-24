b327e05b77a4a1e18d4b9c96967ba99d0139e797
harinichandrasekar11 <harinichandrasekar.11@gmail.com>
2026-09-24 19:02:36 +0530

    Delete ex10 directory

--- ex10/src/main/java/com/example/realtime/repo/SensorReadingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.realtime.repo; 
- 
-import com.example.realtime.model.SensorReading; 
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository; 
- 
-public interface SensorReadingRepository extends 
-ReactiveMongoRepository<SensorReading, String> {  
-}
