a84bfe01b5c797a85992df61345f6b375cfc9208
harinichandrasekar11 <harinichandrasekar.11@gmail.com>
2026-09-24 19:03:10 +0530

    Delete ex9 directory

--- ex9/src/main/java/com/example/employeecrud/repo/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeecrud.repo; 
- 
-import com.example.employeecrud.model.Employee; 
-import org.springframework.data.jpa.repository.JpaRepository; 
- 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> { 
-}
