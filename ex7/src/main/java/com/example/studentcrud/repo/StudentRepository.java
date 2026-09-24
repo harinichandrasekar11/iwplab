818fa25c6efa24fb865386794ea4dbda7c253040
harinichandrasekar11 <harinichandrasekar.11@gmail.com>
2026-09-24 19:02:13 +0530

    Delete ex7 directory

--- ex7/src/main/java/com/example/studentcrud/repo/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentcrud.repo; 
- 
-import com.example.studentcrud.model.Student; 
-import org.springframework.data.jpa.repository.JpaRepository; 
- 
-public interface StudentRepository extends JpaRepository<Student, Long> { 
-}
